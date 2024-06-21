d0c035720494bbdacbf9be892f946a100c9d9f65
Yuvraj Zanwar <yuvrajzanwar@gmail.com>
2024-06-22 00:19:15 +0530

    cors issue solve attempt 4

--- server.js
+++ server.js
@@ -10,13 +10,6 @@ app.use(bodyParser.json());
 
 app.use(cors());
 
-app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:5173');
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-    next();
-});
-
 const transporter = nodemailer.createTransport({
     service: 'gmail',
     auth: {
