ae5f765a96ce3c1c796b4887de0fedd404dd7749
Yuvraj Zanwar <yuvrajzanwar@gmail.com>
2024-06-22 00:10:16 +0530

    cors issue solve attempt 2

--- server.js
+++ server.js
@@ -7,9 +7,12 @@ require('dotenv').config();
 
 const app = express();
 app.use(bodyParser.json());
-app.use(cors({
-    origin: 'http://localhost:5173'
-}));
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:5173');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    next();
+});
 
 const transporter = nodemailer.createTransport({
     service: 'gmail',
